3048ad1451d4d8f463bcbdbc5d99bcce81beca51
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-06-23 12:35:50 +0100

    Solved containerWithMostWater for second time

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -1,21 +1,65 @@
 array = [1, 8, 6, 2, 9, 4]
 
+#-------------------------------------------------------------------------------------
+# 2ND ATTEMPT SOLUTION
+#-------------------------------------------------------------------------------
+#-----------------------
 ## BRUTE FORCE SOLUTION
-def container_with_most_water(heights):
-    most_water = 0 
+#-----------------------
+
+def brute_container_with_most_water(heights):
+    most_water = 0
     array_length = len(heights)
 
     for i in range(array_length):
-        for j in range(i+1, array_length):
-            if j < array_length:
-                height = min(heights[i], heights[j])
-                width = j - i 
-                area = height * width 
-                most_water = max(area, most_water)
+        for j in range(i + 1, array_length):
+
+            min_height = min(heights[i], heights[j])
+            width = j - i 
+            area = min_height * width 
+            most_water = max(area, most_water)
 
     return most_water
 
+#-----------------------
 ## OPTIMAL SOLUTION
+#-----------------------
+
+def optimal_container_with_most_water(heights):
+    l = 0 
+    r = len(heights) - 1 
+    most_water = 0 
+
+    while l < r:
+        min_height = min(heights[l], heights[r])
+        width = r - l 
+        area = min_height * width 
+        most_water = max(area, most_water)
+
+        if min_height == heights[l]:
+            l += 1 
+        else:
+            r -= 1
+
+    return most_water
+
+
+# ## BRUTE FORCE SOLUTION
+# def container_with_most_water(heights):
+#     most_water = 0 
+#     array_length = len(heights)
+
+#     for i in range(array_length):
+#         for j in range(i+1, array_length):
+#             if j < array_length:
+#                 height = min(heights[i], heights[j])
+#                 width = j - i 
+#                 area = height * width 
+#                 most_water = max(area, most_water)
+
+#     return most_water
+
+# ## OPTIMAL SOLUTION
 
 def container_with_most_water_1(heights):
     most_water = 0
@@ -36,17 +80,17 @@ def container_with_most_water_1(heights):
     return most_water
 
 
-print(container_with_most_water(array))
-print(container_with_most_water([]))
-print(container_with_most_water([5]))
-print(container_with_most_water([4, 5]))
-print(container_with_most_water([7, 1, 2, 9]))
+print(brute_container_with_most_water(array))
+print(brute_container_with_most_water([]))
+print(brute_container_with_most_water([5]))
+print(brute_container_with_most_water([4, 5]))
+print(brute_container_with_most_water([7, 1, 2, 9]))
 
-print(container_with_most_water_1(array))
-print(container_with_most_water_1([]))
-print(container_with_most_water_1([5]))
-print(container_with_most_water_1([4, 5]))
-print(container_with_most_water_1([7, 1, 2, 9]))
+print(optimal_container_with_most_water(array))
+print(optimal_container_with_most_water([]))
+print(optimal_container_with_most_water([5]))
+print(optimal_container_with_most_water([4, 5]))
+print(optimal_container_with_most_water([7, 1, 2, 9]))
 
 
 
