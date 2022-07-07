2c2a99fa7bce3a73d8af70c537e9dbdb917ee9c4
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-07 10:33:53 +0100

    Added kthMostElement solution using hoares quickselect algorithm

--- kthMostElement.py
+++ kthMostElement.py
@@ -7,42 +7,89 @@ arr3 = [5]; k3 = 1
 # MY FIRST SOLUTION
 #-------------------------------------------------------------------------------
 
-def quick_sort(array, left, right):
+# def quick_sort(array, left, right):
 
-    if left < right:
+#     if left < right:
 
-        partition_idx = partition(array, left, right)
-        quick_sort(array, left, partition_idx - 1)
-        quick_sort(array, partition_idx + 1, right)
+#         partition_idx = partition(array, left, right)
+#         quick_sort(array, left, partition_idx - 1)
+#         quick_sort(array, partition_idx + 1, right)
 
 
-def partition(array, left, right):
+# def partition(array, left, right):
     
-    pivot_element = array[right]
-    partition_idx = left
+#     pivot_element = array[right]
+#     partition_idx = left
 
-    for j in range(left, len(array)):
+#     for j in range(left, len(array)):
         
-        if array[j] < pivot_element:
-            swap(array, partition_idx, j)
-            partition_idx += 1 
+#         if array[j] < pivot_element:
+#             swap(array, partition_idx, j)
+#             partition_idx += 1 
 
-    swap(array, partition_idx, right)
+#     swap(array, partition_idx, right)
 
-    return partition_idx
+#     return partition_idx
 
+# def swap(array, i, j):
+#     temp = array[i]
+#     array[i] = array[j]
+#     array[j] = temp 
+
+# def get_k_most_element(array, k):
+#     idx_to_find = -k 
+#     quick_sort(array, 0, len(array) - 1)
+#     return array[idx_to_find]
+
+# print(get_k_most_element(arr1, k1))
+# print(get_k_most_element(arr2, k2))
+# print(get_k_most_element(arr3, k3))
+
+#------------------------------------------------------------------------------
+# MY FIRST HOARES QUICKSELECT SOLUTION
+#-------------------------------------------------------------------------------
 def swap(array, i, j):
     temp = array[i]
     array[i] = array[j]
     array[j] = temp 
 
-def get_k_most_element(array, k):
-    idx_to_find = -k 
-    quick_sort(array, 0, len(array) - 1)
-    return array[idx_to_find]
+def partition(array, left, right):
 
-print(get_k_most_element(arr1, k1))
-print(get_k_most_element(arr2, k2))
-print(get_k_most_element(arr3, k3))
+    pivot_element = array[right]
+    partition_idx = left
 
-    
+    for j in range(left, len(array)):
+        if array[j] < pivot_element:
+            swap(array, partition_idx, j) 
+            partition_idx += 1 
+
+    swap(array, partition_idx, right)
+        
+
+    return partition_idx 
+
+def quick_sort(array, left, right, idx_to_find):
+
+    print(array[left : right + 1])
+
+    if left < right:
+
+        partition_idx = partition(array, left, right)
+
+        
+        if (idx_to_find == partition_idx):
+            print(f"Array: {array}, Index to find: {idx_to_find}, value: {array[idx_to_find]}")
+            return array[idx_to_find]
+        elif idx_to_find < partition_idx:
+            quick_sort(array, left, partition_idx - 1, idx_to_find)
+        else:
+            quick_sort(array, partition_idx + 1, right, idx_to_find)
+
+
+
+print(quick_sort(arr1, 0, len(arr1) - 1, len(arr1) - k1))
+
+
+
+
+
