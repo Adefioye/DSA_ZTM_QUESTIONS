e54d16fb2e2ab486a8f1df5839809128ea33a896
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-08 22:25:27 +0100

    Implemented reverse linked list for second time

--- reverseALinkedList.py
+++ reverseALinkedList.py
@@ -1,53 +1,77 @@
-class node:
-    def __init__(self, value):
-        self.value = value 
-        self.next = None 
-        self.length = 0
+#-------------------------------------------------------------------------------------
+# MY FIRST SOLUTION
+#-------------------------------------------------------------------------------
 
-class linked_list:
-    def __init__(self):
-        self.head = None 
-        self.tail = None
-        self.length = 0 
+# class node:
+#     def __init__(self, value):
+#         self.value = value 
+#         self.next = None 
+#         self.length = 0
 
-    def push(self, value):
-        new_node = node(value)
+# class linked_list:
+#     def __init__(self):
+#         self.head = None 
+#         self.tail = None
+#         self.length = 0 
 
-        if self.head == None:
-            self.head = new_node 
-            self.tail = self.head 
+#     def push(self, value):
+#         new_node = node(value)
 
-        self.tail.next = new_node 
-        self.tail = new_node
-        self.length += 1 
+#         if self.head == None:
+#             self.head = new_node 
+#             self.tail = self.head 
 
-    def reverse(self):
+#         self.tail.next = new_node 
+#         self.tail = new_node
+#         self.length += 1 
 
-        current_node = self.head 
-        prev_node = None 
-        self.tail = self.head
+#     def reverse(self):
 
-        while current_node:
-            next_node = current_node.next 
-            current_node.next = prev_node
-            prev_node = current_node
-            current_node = next_node
+#         current_node = self.head 
+#         prev_node = None 
+#         self.tail = self.head
 
-        self.head = prev_node 
+#         while current_node:
+#             next_node = current_node.next 
+#             current_node.next = prev_node
+#             prev_node = current_node
+#             current_node = next_node
 
-    def print(self):
-        current_node = self.head 
+#         self.head = prev_node 
 
-        while current_node:
-            print(current_node.value)
-            current_node = current_node.next 
+#     def print(self):
+#         current_node = self.head 
+
+#         while current_node:
+#             print(current_node.value)
+#             current_node = current_node.next 
 
 
-linked_list = linked_list()
-linked_list.push(5)
-linked_list.push(8)
-linked_list.push(7)
-linked_list.reverse()
-linked_list.print()
+# linked_list = linked_list()
+# linked_list.push(5)
+# linked_list.push(8)
+# linked_list.push(7)
+# linked_list.reverse()
+# linked_list.print()
+#-------------------------------------------------------------------------------------
+# MY SECOND SOLUTION
+#-------------------------------------------------------------------------------
+
+def reverse_linked_list(head):
+
+    current_node = head 
+    prev_node = None 
+    tail = head 
+
+    while current_node:
+        temp = current_node.next 
+        current_node.next = None 
+        prev_node = current_node 
+        current_node = temp 
+
+    head = prev_node
+
+    return head 
+
 
 
