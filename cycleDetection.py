d5d81e56780444782f0017c70a9598e919654353
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-12 21:51:03 +0100

    Finished cycle detection brute and optimal solution for second time

--- cycleDetection.py
+++ cycleDetection.py
@@ -1,27 +1,71 @@
+# def brute_cycle_detection(head):
+
+#     current_node = head 
+#     my_set = set() 
+
+#     while(current_node.value not in my_set):
+#         if current_node.next == None:
+#             return False 
+
+#         my_set.add(current_node.value)
+#         current_node = current_node.next
+
+#     return current_node
+
+
+# # Optimal solution is based on tortoise and hare algorithm 
+# def optimal_cycle_detection(head):
+
+#     hare = head 
+#     tortoise = head 
+
+#     while True:
+#         hare = head.next
+#         tortoise = head.next 
+
+#         if hare == None or hare.next == None:
+#             return False 
+#         else:
+#             hare = hare.next 
+
+#         if hare == tortoise:
+#             break
+
+#     p1 = head 
+#     p2 = hare 
+
+#     while p1 != p2:
+        
+#         p1 = p1.next 
+#         p2 = p2.next 
+
+#     return p1
+
+#-------------------------------------------------------------------------------------
+# 2ND SOLUTION
+#-------------------------------------------------------------------------------
 def brute_cycle_detection(head):
-
     current_node = head 
-    my_set = set() 
+    my_set = set()
 
-    while(current_node.value not in my_set):
-        if current_node.next == None:
-            return False 
+    while current_node:
 
-        my_set.add(current_node.value)
+        if current_node.value not in my_set:
+            my_set.add(current_node.value)
+        else:
+            return True 
+
         current_node = current_node.next
 
-    return current_node
+    return False 
 
-
-# Optimal solution is based on tortoise and hare algorithm 
 def optimal_cycle_detection(head):
-
     hare = head 
     tortoise = head 
 
     while True:
-        hare = head.next
-        tortoise = head.next 
+        hare = hare.next 
+        tortoise = hare.next
 
         if hare == None or hare.next == None:
             return False 
@@ -29,7 +73,7 @@ def optimal_cycle_detection(head):
             hare = hare.next 
 
         if hare == tortoise:
-            break
+            break 
 
     p1 = head 
     p2 = hare 
@@ -37,6 +81,7 @@ def optimal_cycle_detection(head):
     while p1 != p2:
         
         p1 = p1.next 
-        p2 = p2.next 
+        p2 = p2.next
 
-    return p1
+    return p1 
+
