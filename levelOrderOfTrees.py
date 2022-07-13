32de563fb78154457094c8cabbae6f2194d8c0ad
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-13 06:57:36 +0100

    Added ZTM solution for level order of trees for first time

--- levelOrderOfTrees.py
+++ levelOrderOfTrees.py
@@ -29,3 +29,33 @@ def level_order_trees(root):
         res.append(array_by_level)
 
     return res
+
+#------------------------------------------------------------------------------
+# ZTM FIRST SOLUTION
+#-------------------------------------------------------------------------------
+
+
+def level_order_trees(root):
+
+    res = []
+    queue = [root]
+
+    while len(queue) > 0:
+        count = 0
+        length = len(queue)
+        current_level_arrays = []
+
+        while count < length:
+            current_node = q.pop(0)
+            current_level_arrays.append(current_node.value)
+            count += 1
+
+            if current_node.left:
+                queue.append(current_node.left)
+
+            if current_node.right:
+                queue.append(current_node.right)
+
+        res.append(current_level_arrays)
+
+    return res
--- play.py
+++ play.py
@@ -2,5 +2,7 @@
 # print(ls[-1])
 
 ## Fiddiling arround
-array = [None, None]
+array = [1, None]
+print(len(array))
+array.pop(0)
 print(len(array))
