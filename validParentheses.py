67e3f409384fd24ec5da2a33d56bd206690e8f94
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-13 08:51:45 +0100

    Added second solution for validParentheses problem

--- validParentheses.py
+++ validParentheses.py
@@ -4,39 +4,78 @@ S3 = "{([]"     # FALSE
 S4 = "{[(])}"   # FALSE
 S5 = "{[]()}"   # TRUE 
 
+#-------------------------------------------------------------------------------------
+# SECOND SOLUTION
+#-------------------------------------------------------------------------------
+
 def valid_parentheses(S):
+
     parentheses = {
-        ")" : "(",
-        "}" : "{",
-        "]" : "["
+        "{": "}",
+        "[": "]",
+        "(": ")"
     }
 
-    # print(parentheses.values())
-    # print(parentheses.keys())
+    if S == "":
+        return True 
 
-    string_length = len(S)
-    stack = [] 
+    stack = []
 
-    if len(S) == 0:
-        return True
-
-    for i in range(string_length):
-
+    for i in range(len(S)):
         current_char = S[i]
 
-        if current_char in parentheses.values():
+        if current_char in parentheses:
             stack.append(current_char)
-        elif current_char in parentheses.keys():
-            # Check if Stack has balancing pair 
+        else:
+            last_open_bracket = stack[-1]
+            matching_close_bracket = parentheses[last_open_bracket]
+
             if len(stack) == 0:
                 return False 
-            elif stack[-1] != parentheses[current_char]:
-                return False
+            elif matching_close_bracket != current_char:
+                return False 
             else:
-                stack.pop() 
+                stack.pop()
+
+    return True if len(stack) == 0 else False
 
 
-    return True if len(stack) == 0 else False 
+#-------------------------------------------------------------------------------------
+# FIRST SOLUTION
+#-------------------------------------------------------------------------------
+# def valid_parentheses(S):
+#     parentheses = {
+#         ")" : "(",
+#         "}" : "{",
+#         "]" : "["
+#     }
+
+#     # print(parentheses.values())
+#     # print(parentheses.keys())
+
+#     string_length = len(S)
+#     stack = [] 
+
+#     if len(S) == 0:
+#         return True
+
+#     for i in range(string_length):
+
+#         current_char = S[i]
+
+#         if current_char in parentheses.values():
+#             stack.append(current_char)
+#         elif current_char in parentheses.keys():
+#             # Check if Stack has balancing pair 
+#             if len(stack) == 0:
+#                 return False 
+#             elif stack[-1] != parentheses[current_char]:
+#                 return False
+#             else:
+#                 stack.pop() 
+
+
+#     return True if len(stack) == 0 else False 
 
 print(valid_parentheses(S1))
 print(valid_parentheses(S2))
