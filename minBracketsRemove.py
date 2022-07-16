6465683d253bdcb92fe2c438ad4ac5c1efe4f395
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-16 11:56:53 +0100

    Added second solution minBracketsRemove

--- minBracketsRemove.py
+++ minBracketsRemove.py
@@ -2,6 +2,51 @@ S1 = "a)bc(d)"
 S2 = "(ab(c)d" 
 S3 = "))(("
 
+
+
+
+
+#-------------------------------------------------------------------------------------
+# MY SECOND SOLUTION
+#---------------------------------------------------------------------------
+# SOLUTION 1
+def remove_min_brackets(S):
+    stack_open = []
+    stack_close = []
+    string_list = list(S)
+    inf = "inf"
+
+    for i in range(len(S)):
+        current_char = S[i]
+
+        if current_char == "(":
+            stack_open.append(i) 
+        elif current_char == ")":
+
+            if len(stack_open) > 0:
+                stack_open.pop()
+            else:
+                stack_close.append(i)
+
+    
+    while len(stack_open) > 0:
+        idx = stack_open.pop()
+        string_list[idx] = inf 
+    
+    while len(stack_close) > 0:
+        idx = stack_close.pop()
+        string_list[idx] = inf 
+
+    while inf in string_list:
+        idx = string_list.index(inf)
+        string_list.pop(idx)
+        
+    return "".join(string_list)
+
+print(remove_min_brackets(S1))
+print(remove_min_brackets(S2))
+print(remove_min_brackets(S3))
+
 #-------------------------------------------------------------------------------------
 # MY FIRST SOLUTION
 #-------------------------------------------------------------------------------
@@ -36,6 +81,6 @@ def min_brackets_remove(S):
             
     return "".join(stack)
 
-print(min_brackets_remove(S1))
-print(min_brackets_remove(S2))
-print(min_brackets_remove(S3))
+# print(min_brackets_remove(S1))
+# print(min_brackets_remove(S2))
+# print(min_brackets_remove(S3))
