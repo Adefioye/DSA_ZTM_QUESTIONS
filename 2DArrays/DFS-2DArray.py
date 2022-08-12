217f73208f9a9696703c53b96327fb5bd8b0bf6c
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-08-12 11:59:03 -0500

    Added DFS 2D traversal

---
+++ 2DArrays/DFS-2DArray.py
@@ -0,0 +1,31 @@
+testMatrix = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20]
+];
+
+def twoD_dfs(matrix):
+    row_size = len(matrix)
+    col_size = len(matrix[0])
+    visited = set()
+    result = []
+
+    def dfs(row, col, visited):
+      result.append(matrix[row][col])  
+      visited.add((row, col))
+      directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+      for dir in directions:
+        r = row + dir[0]
+        c = col + dir[1]
+
+        if r < 0 or r >= row_size or c < 0 or c >= col_size or (r, c) in visited:
+          continue
+
+        dfs(r, c, visited)
+
+    dfs(0, 0, visited)
+    return result
+
+print(twoD_dfs(testMatrix))
